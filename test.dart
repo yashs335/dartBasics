db21da6dbdbde3bbc0e1c54cb59ee1623dcf4fe1
yashs335 <yashs@webmobtech.com>
2025-05-21 12:24:12 +0530

    TEST01
    list test sorting,convert multidementinal list to single and seprate comman from two list without using the loop

---
+++ test.dart
@@ -0,0 +1,46 @@
+void main() {
+  var number_sets = [
+    [1, 2, 3, 4],
+    [3, 4, 5, 6],
+    [7, 8, 9],
+    [1, 2, 3, 4],
+    [10, 11],
+    [5, 6, 7],
+    [12, 13, 14],
+    [8, 9, 10],
+    [],
+    [100, 200, 300],
+    [300, 400],
+    [500],
+    [13, 14, 15],
+    [0, -1, -2],
+    [-1, -2, -3],
+    [999],
+    [1],
+    [42, 43, 44, 45, 46],
+    [44, 45],
+    [999],
+  ];
+
+  List<int> list = [];
+
+  // number_sets.forEach((e) => e.forEach((i) => list.add(i)));
+  number_sets.every(
+    (element) => element.every((e) {
+      list.add(e);
+      return true;
+    }),
+  );
+
+  // for (var element in number_sets) {
+  //   if (element.isNotEmpty) {
+  //     for (var insideElement in element) {
+  //       list.add(insideElement);
+  //     }
+  //   }
+  // }
+
+  print("list without sorting : $list");
+  list.sort();
+  print("list with sorting : $list");
+}
---
+++ test2.dart
@@ -0,0 +1,24 @@
+/*Take two lists, for example:
+  a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+
+  b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+and write a program that returns a list that contains only the elements that are common between them 
+(without duplicates). Make sure your program works on two lists of different sizes.
+*/
+
+void main() {
+  List<int> a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
+  List<int> b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+  List newList = [];
+
+  // newList = a.where((e) => b.contains(e)).toList();
+
+  var res = a.every((element) {
+    if (b.contains(element)) {
+      if (!newList.contains(element)) newList.add(element);
+    }
+    return true;
+  });
+
+  print(newList);
+}
